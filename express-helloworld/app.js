b95ec5f607532fb8eb5a62deb76698f6113c7642
Yu Zhong <zxx.1995@gmail.com>
2023-09-05 15:29:05 +0200

    Add "check it"

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -9,6 +9,10 @@ app.get('/jim', function (req, res) {
   res.send('Jim greets you.\n');
 });
 
+app.get('/check', function (req, res) {
+  res.send('<h1>Check it!</h1>');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
