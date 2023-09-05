dc2fd528826efa1e1592946623f4006a9cc3b811
Yu Zhong <zxx.1995@gmail.com>
2023-09-05 14:50:23 +0200

    Add Jim's greeting

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
 
+app.get('/jim', function (req, res) {
+  res.send('Hello Jim OpenShift Test.\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
